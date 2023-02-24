089943d60660b90a81095f56bb77df0dc5bd18d2
Andrii <andriyshopyak@gmail.com>
2023-02-24 10:45:19 +0200

    feat: add corect solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.a = min;
+        this.b = max;
     }
 
     guess() {
-
+        this.sum = Math.round((this.b + this.a) / 2);
+        return this.sum;
     }
 
     lower() {
-
+        this.b = this.sum;
     }
 
     greater() {
-
+        this.a = this.sum;
     }
 }
 
